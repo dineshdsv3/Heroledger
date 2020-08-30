e1cfdc8a0266ff6435771e02dac238b24ace0fed
dineshdsv3 <dineshdsv3@gmail.com>
2020-08-30 14:16:58 +0530

    build version

--- Backend/app.js
+++ Backend/app.js
@@ -6,12 +6,17 @@ var server = http.Server(app);
 const userRouter = require('./routers/user')
 const port = process.env.PORT
 require('./db/db')
-
+const staticFilesPath = path.join(process.cwd(), 'build');
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use(express.json())
+app.use(express.static(staticFilesPath));
+
+app.use(function (req, res) {
+	res.sendFile(path.join(staticFilesPath, 'index.html'));
+});
 app.use(userRouter)
 
 app.listen(port, () => {
